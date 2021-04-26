d8ae766a4d4814e86d98bd6ab4bd5ec844405fc0
Luanaap <l.andrade2711@gmail.com>
2021-04-25 21:49:46 -0300

    Adicionar arquivos de projeto.

---
+++ Models/CandidatoViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrnaEletronica.Models
+{
+    public class CandidatoViewModels
+    {
+        public class CandidatoViewModel
+        {
+            public string NomeCompleto { get; set; }
+            public string Vice { get; set; }
+            public DateTime Inscricao { get; set; }
+            public int Legenda { get; set; }
+        }
+    }
+}
